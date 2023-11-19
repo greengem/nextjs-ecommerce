0bf7899dc5b0af653f282f93264c50bc60a6d250
Green Gem <chris_waitt@me.com>
2023-11-19 19:34:21 +0000

    Project Init - Installed prisma, nextauth v5 beta, react-hot-toast. - Set up rough routing structure - Removed default template - Next Auth is using prisma adapter with a posfres db. - Prisma schema for nextauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Toaster } from 'react-hot-toast';
+import AppNavbar from './_components/AppNavbar';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Toaster />
+        <AppNavbar />
+        {children}
+      </body>
     </html>
   )
 }
