da119f032fad9c652d4abcd7006added1600a4f5
Green Gem <chris_waitt@me.com>
2023-11-20 16:03:11 +0000

    Product Page fetching

---
+++ app/products/[slug]/page.tsx
@@ -0,0 +1,69 @@
+import prisma from '@/db/prisma';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Product } from '@/types/ProductTypes';
+
+async function getProduct(slug: string): Promise<Product | null> {
+    const product = await prisma.product.findUnique({
+        where: {
+            slug: slug,
+        },
+        select: {
+            id: true,
+            name: true,
+            slug: true,
+            price: true,
+            description: true,
+            image: true,
+            categories: {
+                select: {
+                    id: true,
+                    name: true,
+                    slug: true,
+                    image: true,
+                }
+            }
+        }
+    });
+    return product;
+}
+
+export default async function Product({ params }: { params: { slug: string } }) {
+    const product = await getProduct(params.slug);
+
+    if (!product) {
+        return <div>Product not found</div>;
+    }
+
+    return (
+        <>
+            {product.image ? (
+                <Image 
+                    src={product.image} 
+                    alt={product.name} 
+                    width={600} height={600}
+                    className='mb-1'
+                />
+            ) : (
+                <Image 
+                    src="https://loremflickr.com/600/600" 
+                    alt='placeholder' 
+                    width={600} height={600}
+                    className='mb-1'
+                />
+            )}
+            <p>Name: {product.name}</p>
+            <p className='text-xs'>{product.description}</p>
+            <p className='text-xs font-bold'>£{product.price}</p>
+            <ul>
+                {product.categories.map(category => (
+                    <li key={category.id}>
+                        <Link href={`/category/${category.slug}`}>
+                            {category.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
+}
