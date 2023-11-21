c2f5b9a2154e26f27e7bc1d02f2e5199904660d4
Green Gem <chris_waitt@me.com>
2023-11-21 16:27:32 +0000

    Get Tag name from tag slug for page titles

--- app/tag/[slug]/page.tsx
+++ app/tag/[slug]/page.tsx
@@ -1,9 +1,23 @@
+import prisma from '@/db/prisma';
 import { getProductsByTag } from '@/lib/FetchData';
 import PageHeading from '@/ui/Heading/PageHeading';
 import ProductItem from '@/ui/Product/ProductItem';
 
+async function getTagName(slug: string) {
+    const tag = await prisma.tag.findUnique({
+        where: {
+            slug: slug,
+        },
+        select: {
+            name: true,
+        }
+    });
+    return tag ? tag.name : '';
+}
+
 export default async function ProductsByTag({ params }: { params: { slug: string } }) {
-    const { products, tagName } = await getProductsByTag(params.slug);
+    const tagName = await getTagName(params.slug)
+    const products = await getProductsByTag(params.slug);
 
     if (!products) {
         return <div>Tag not found</div>;
