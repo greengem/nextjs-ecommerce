0e12c54d79cf616e5d9984567e3e1e8184c239b1
Green Gem <chris_waitt@me.com>
2023-11-21 13:08:35 +0000

    Our server action that takes our form data, validates it with zod, creates a new prisma record and redirects the user. this needs more validation, loading states and toasts for outcome. but currently the form is working completely on the server

---
+++ app/actions.ts
@@ -0,0 +1,62 @@
+'use server';
+
+import { z } from 'zod'
+import prisma from '@/db/prisma';
+import { redirect } from "next/navigation";
+
+const ProductSchema = z.object({
+	productName: z.string(),
+	productSlug: z.string(),
+	productDescription: z.string(),
+	productPrice: z.number(),
+	productCategory: z.string(),
+	productTags: z.array(z.string()),
+	productImage: z.instanceof(File).optional(),
+});
+
+export async function handleCreateNewProduct(formData: FormData) {
+  const productName = formData.get('productName') as string;
+  const productSlug = formData.get('productSlug') as string;
+  const productDescription = formData.get('productDescription') as string;
+  const productPrice = parseFloat(formData.get('productPrice') as string) || 0;
+  const productCategory = formData.get('productCategory') as string;
+  const productTags = formData.getAll('productTags') as string[];
+  const productImage = formData.get('productImage') as File | string;
+
+  const productData = {
+    productName,
+    productSlug,
+    productDescription,
+    productPrice,
+    productCategory,
+    productTags,
+    productImage,
+  };
+
+  try {
+    const validatedData = ProductSchema.parse(productData);
+    
+    console.log(validatedData);
+
+    const product = await prisma.product.create({
+      data: {
+        name: validatedData.productName,
+        slug: validatedData.productSlug,
+        description: validatedData.productDescription,
+        price: validatedData.productPrice,
+        categories: {
+          connect: { slug: validatedData.productCategory },
+        },
+        tags: {
+          connect: validatedData.productTags.map(tagSlug => ({ slug: tagSlug })),
+        },
+      }
+    });
+
+    redirect("/admin/products");
+
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
