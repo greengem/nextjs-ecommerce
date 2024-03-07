dd7d91d29e6c484c6a7ea0bf2f60f7225d8abb1a
Green Gem <chris_waitt@me.com>
2024-03-07 23:39:23 +0000

    Refactor handleCreateNewProduct function to use raw form data

--- app/actions/ProductActions.ts
+++ app/actions/ProductActions.ts
@@ -4,11 +4,35 @@ import { z } from 'zod'
 import prisma from '@/db/prisma';
 import { revalidatePath } from 'next/cache'
 
-export async function handleCreateNewProduct(data: FormData) {
-  console.log('server data', data);
+export async function handleCreateNewProduct(formData: FormData) {
+  const rawFormData = {
+    name: formData.get('name'),
+    slug: formData.get('slug'),
+    status: formData.get('price'),
+    description: formData.get('description'),
+    category: formData.get('category'),
+    tags: formData.get('tags'),
+    inventory: formData.get('inventory'),
+  }
 
   try {
-
+    await prisma.product.create({
+      data: {
+        name: rawFormData.name,
+        slug: rawFormData.slug,
+        price: rawFormData.price,
+        description: rawFormData.description,
+        category: {
+          connect: {
+            slug: rawFormData.category
+          }
+        },
+        tags: {
+          connect: rawFormData.tags.map((tag: string) => ({ slug: tag }))
+        },
+        inventory: rawFormData.inventory
+      }
+    });
     revalidatePath('/admin/products');
     return { success: true, message: "Product Created!"};
   } catch (e) {
