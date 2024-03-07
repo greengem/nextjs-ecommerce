b1374461d767cce6299b90ffc6c3f389f7bf1fd1
Green Gem <chris_waitt@me.com>
2024-03-07 17:37:24 +0000

    Add admin dashboard link to AdminMenu component

--- ui/Navbar/AdminMenu.tsx
+++ ui/Navbar/AdminMenu.tsx
@@ -1,12 +1,19 @@
+import { redirect } from "next/navigation";
+import { checkRole } from "@/utils/roles";
 import Link from "next/link";
 
 export default function AdminMenu() {
+
+    if (!checkRole("admin")) {
+        return null;
+    }
+
     return (
         <nav className="px-5 py-1 bg-red-500 text-white text-xs">
             <ul className="flex gap-2">
                 <li>
-                    <Link href="/admin">
-                        Admin Home
+                    <Link href="/admin/dashboard">
+                        Dashboard
                     </Link>
                 </li>
                 <li>
