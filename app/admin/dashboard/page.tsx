f5f94d406eca054d9687d094afbc5b913877ee3b
Green Gem <chris_waitt@me.com>
2024-03-08 14:17:55 +0000

    Update admin dashboard page to fix role description

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -19,7 +19,7 @@ export default async function AdminDashboard(params: {
   return (
     <>
       <h1>This is the admin dashboard</h1>
-      <p>This page is restricted to users with the 'admin' role.</p>
+      <p>This page is restricted to users with the admin role.</p>
  
       <SearchUsers />
  
