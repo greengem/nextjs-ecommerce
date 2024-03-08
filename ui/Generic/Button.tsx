f835aa428382a2116fa3d4d3017c49a4ae4a6d78
Green Gem <chris_waitt@me.com>
2024-03-08 16:11:45 +0000

    Update Button component to support disabled state

--- ui/Generic/Button.tsx
+++ ui/Generic/Button.tsx
@@ -1,18 +1,24 @@
 'use client'
+import clsx from 'clsx';
 import { useFormStatus } from 'react-dom'
 
 type ButtonProps = {
     children: React.ReactNode;
     onClick?: () => void;
     type?: "button" | "submit" | "reset";
+    disabled?: boolean;
 };
   
-export function Button({ children, onClick, type }: ButtonProps) {
+export function Button({ children, onClick, type, disabled }: ButtonProps) {
   return (
     <button
       type={type || "button"}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs"
+      className={clsx(
+        "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs",
+        { "opacity-50": disabled }
+      )}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
@@ -22,6 +28,6 @@ export function Button({ children, onClick, type }: ButtonProps) {
 export function SubmitButton({ children }: ButtonProps) {
   const { pending } = useFormStatus()
   return (
-    <Button type="submit" aria-disabled={pending}>{children}</Button>
+    <Button type="submit" disabled={pending}>{children}</Button>
   );
 }
