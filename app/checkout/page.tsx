f287b3a1e4fed4dbe72bb80b8b9c4b685d9dc35f
Green Gem <chris_waitt@me.com>
2024-03-09 12:26:01 +0000

    Add cart items table to checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,38 @@
+import { auth } from "@clerk/nextjs";
+import prisma from "@/db/prisma";
 import PageHeading from "@/ui/Heading/PageHeading";
+import Table from "@/ui/Generic/Table";
+import RemoveFromCartButton from "../cart/_components/RemoveFromCartButton";
 
-export default function Checkout() {
+export default async function Checkout() {
+    const { userId } = auth();
+
+    if (!userId) {
+        throw new Error("Unauthorized");
+    }
+
+    const cartItems = await prisma.cartItem.findMany({
+        where: {
+            cart: {
+                userId: userId
+            }
+        },
+        include: {
+            product: true,
+        },
+    });
+
+    const headers = ['Product Name', 'Quantity', 'Actions'];
+    const data = cartItems.map(item => [
+        item.product.name, 
+        item.quantity, 
+        <RemoveFromCartButton cartItemId={item.id} />
+    ]);
+
     return (
-        <PageHeading title='Checkout' />
+        <>
+            <PageHeading title='Checkout' />
+            <Table headers={headers} data={data} />
+        </>
     )
 }
