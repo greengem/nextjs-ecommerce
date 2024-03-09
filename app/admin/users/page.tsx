69544a2487aa7e3644a70bb81e2d9c484ff1ecd6
Green Gem <chris_waitt@me.com>
2024-03-09 15:21:17 +0000

    Remove unused code in AdminUsersPage

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,61 +1,9 @@
-import Image from 'next/image';
 import PageHeading from '@/ui/Heading/PageHeading';
-import { 
-	AdminTable, 
-	AdminTableHeader, 
-	AdminTableHeaderItem, 
-	AdminTableBody, 
-	AdminTableBodyRow, 
-	AdminTableBodyRowItem 
-} from '@/ui/Admin/Table';
-import { Button } from '@/ui/Generic/Button';
 
 export default async function AdminUsersPage() {
 	return (
 		<>
 			<PageHeading title='Users' />
-			
-			<AdminTable>
-				<AdminTableHeader>
-					<AdminTableHeaderItem>Select</AdminTableHeaderItem>
-					<AdminTableHeaderItem>Image</AdminTableHeaderItem>
-					<AdminTableHeaderItem>Name</AdminTableHeaderItem>
-					<AdminTableHeaderItem>Email</AdminTableHeaderItem>
-					<AdminTableHeaderItem>Actions</AdminTableHeaderItem>
-				</AdminTableHeader>
-				<AdminTableBody>
-					Todo
-					{/* {users.map((user) => (
-						<AdminTableBodyRow key={user.id}>
-							<AdminTableBodyRowItem><input type='checkbox' /></AdminTableBodyRowItem>
-							<AdminTableBodyRowItem>
-								<>
-									{user.image ? (
-										<Image 
-											src={user.image} 
-											alt={user.name || 'User Image'}
-											width={32} height={32}
-											className='mb-1 rounded-full'
-										/>
-									) : (
-										<Image 
-											src="https://loremflickr.com/60/60" 
-											alt='placeholder' 
-											width={32} height={32}
-											className='mb-1 rounded-full'
-										/>
-									)}
-								</>
-							</AdminTableBodyRowItem>
-							<AdminTableBodyRowItem>{user.name}</AdminTableBodyRowItem>
-							<AdminTableBodyRowItem>{user.email}</AdminTableBodyRowItem>
-							<AdminTableBodyRowItem>
-								<Button>Edit</Button>
-							</AdminTableBodyRowItem>
-						</AdminTableBodyRow>
-					))} */}
-				</AdminTableBody>
-			</AdminTable>
 		</>
 	)
 }
