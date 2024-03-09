b1cd4a369b32268c2b4c93ce3765f25380dd17c0
Green Gem <chris_waitt@me.com>
2024-03-09 09:14:46 +0000

    Add authentication and display cart items in Cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,36 @@
+import { auth } from "@clerk/nextjs";
 import PageHeading from "@/ui/Heading/PageHeading";
+import prisma from "@/db/prisma";
+import RemoveFromCartButton from "./_components/RemoveFromCartButton";
 
-export default function Cart() {
+export default async function Cart() {
+    const { userId } = auth();
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const cartItems = await prisma.cartItem.findMany({
+        where: {
+            cart: {
+                userId: userId
+            }
+        },
+        include: {
+            product: true,
+    });
+
     return (
+        <>
         <PageHeading title='Cart' />
+        <ul>
+            {cartItems.map((item) => (
+            <li key={item.id}>
+                <p>{item.product.name} - Quantity: {item.quantity}</p>
+                <RemoveFromCartButton cartItemId={item.id} />
+            </li>
+            ))}
+        </ul>
+        </>
     )
 }
