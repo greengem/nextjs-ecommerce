e934a4865ae71a0b17cf1ccfcaf262563b50b2c2
Green Gem <chris_waitt@me.com>
2024-03-09 09:15:16 +0000

    Add new component for adding items to cart

---
+++ ui/Product/AddToCart.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { handleAddToCart } from "@/server-actions/CartActions";
+import { Button } from "../Generic/Button";
+import { useTransition } from "react";
+
+export default function AddToCart({ productId } : { productId: string }) {
+    const [isPending, startTransition] = useTransition();
+
+    const handleClick = () => {
+        startTransition(() => {
+            handleAddToCart(productId);
+        });
+    };
+
+    return (
+        <Button onClick={handleClick} disabled={isPending}>
+            Add to Cart
+        </Button>
+    )
+}
