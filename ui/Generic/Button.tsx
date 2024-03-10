841a17c502d1f89202a1fdbcf5c4ee45a3d70a09
Green Gem <chris_waitt@me.com>
2024-03-10 12:10:32 +0000

    Refactor Button component to use cn utility function

--- ui/Generic/Button.tsx
+++ ui/Generic/Button.tsx
@@ -1,5 +1,5 @@
 'use client'
-import clsx from 'clsx';
+import { cn } from '@/lib/utils';
 import { useFormStatus } from 'react-dom'
 
 type ButtonProps = {
@@ -8,15 +8,17 @@ type ButtonProps = {
     type?: "button" | "submit" | "reset";
     disabled?: boolean;
     isIconOnly?: boolean;
+    className?: string;
 };
   
-export function Button({ children, onClick, type, disabled, isIconOnly }: ButtonProps) {
+export function Button({ children, onClick, type, disabled, isIconOnly, className }: ButtonProps) {
   return (
     <button
       type={type || "button"}
-      className={clsx(
-        "bg-neutral-100 hover:bg-neutral-400 text-black font-bold py-2 rounded text-xs ring-1 ring-neutral-500",
-        { "opacity-50": disabled, "px-2": isIconOnly, "px-4": !isIconOnly }
+      className={cn(
+        "bg-neutral-100 hover:bg-neutral-400 text-black font-bold py-2 rounded text-xs ring-1 ring-neutral-400",
+        { "opacity-50": disabled, "px-2": isIconOnly, "px-4": !isIconOnly },
+        className
       )}
       onClick={onClick}
       disabled={disabled}
