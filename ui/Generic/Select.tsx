2597a3d78b493a13884f1cd4b2f3265194d2cd87
Green Gem <chris_waitt@me.com>
2024-03-10 12:10:38 +0000

    Update Select component to support multiple selections

--- ui/Generic/Select.tsx
+++ ui/Generic/Select.tsx
@@ -3,7 +3,7 @@ import { SelectHTMLAttributes } from 'react';
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
     label?: string;
     id?: string;
-    value?: string;
+    value?: string | string[];
     onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
     required?: boolean;
     children?: React.ReactNode;
@@ -13,7 +13,7 @@ export default function Select({ label, id, value, onChange, required, children,
     return (
         <div className='mb-4'>
             <label htmlFor={id} className='block text-gray-600 font-semibold'>{label}</label>
-            <select name={id} id={id} value={value} onChange={onChange} required={required} className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500" {...props}>
+            <select multiple={Array.isArray(value)} name={id} id={id} value={value} onChange={onChange} required={required} className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500" {...props}>
                 {children}
             </select>
         </div>
