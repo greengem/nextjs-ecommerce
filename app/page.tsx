c7ad38cf002ba868e5b85eacf48e6d6783f72e70
Green Gem <chris_waitt@me.com>
2024-03-10 14:42:21 +0000

    Update New Arrivals section layout

--- app/page.tsx
+++ app/page.tsx
@@ -40,9 +40,9 @@ export default async function HomePage() {
 			</div>
             
             <div className="p-5">
-                <h2 className="text-3xl font-semibold tracking-tight text-center mb-3">New Arrivals</h2>
+                <h2 className="text-2xl font-semibold tracking-tight mb-5">New Arrivals</h2>
 
-                <div className='grid grid-cols-1 md:grid-cols-5 gap-5'>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
                     {products.map((product) => (
                         <ProductItem key={product.id} product={product} />
                     ))}
