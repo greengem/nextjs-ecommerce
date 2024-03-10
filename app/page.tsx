e9c2e9902feb6042de4a2f11684cab148e83234d
Green Gem <chris_waitt@me.com>
2024-03-10 12:09:47 +0000

    Add Tranquiluxe component and update styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import prisma from "@/db/prisma";
+import PageHeading from "@/ui/Heading/PageHeading";
 import ProductItem from "@/ui/Product/ProductItem";
+import { Tranquiluxe } from "uvcanvas"
+
+
 export default async function HomePage() {
 
 	const products = await prisma.product.findMany({
@@ -30,14 +34,19 @@ export default async function HomePage() {
 
 	return (
 		<>
-			<div className="h-96 bg-neutral-200 flex flex-col justify-center items-center">
-				<h2 className="text-6xl font-semibold tracking-tighter">Spring Collection</h2>
+			<div className="h-96 flex flex-col justify-center items-center relative">
+                <Tranquiluxe />
+				<h2 className="absolute text-7xl font-semibold tracking-tighter text-white">Spring Collection</h2>
 			</div>
+            
+            <div className="p-5">
+                <h2 className="text-3xl font-semibold tracking-tight text-center mb-3">New Arrivals</h2>
 
-            <div className='p-5 grid grid-cols-1 md:grid-cols-5 gap-5'>
-                {products.map((product) => (
-                    <ProductItem key={product.id} product={product} />
-                ))}
+                <div className='grid grid-cols-1 md:grid-cols-5 gap-5'>
+                    {products.map((product) => (
+                        <ProductItem key={product.id} product={product} />
+                    ))}
+                </div>
             </div>
 		</>
 	);
