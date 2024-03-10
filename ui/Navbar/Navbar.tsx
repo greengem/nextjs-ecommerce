4190d34bebe5e0b6ab43fd3b983cb920bf087658
Green Gem <chris_waitt@me.com>
2024-03-10 12:10:44 +0000

    Refactor Navbar component

---
+++ ui/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import CartDrawer from "../CartDrawer/CartDrawer";
+import { IconFlame } from "@tabler/icons-react";
+import NavbarMenu from "./NavbarMenu";
+
+function NavbarBrand() {
+    const brandLogo = <IconFlame />;
+    return (
+        <Link href="/" className="text-xl tracking-tight font-light flex items-center">
+            {brandLogo} {process.env.NEXT_PUBLIC_BRAND_NAME}
+        </Link>
+    )
+}
+
+export default function Navbar() {
+
+    return (
+        <nav className="px-5 py-3 bg-black text-white grid grid-cols-[100px,1fr,100px]">
+            <NavbarBrand />
+            <NavbarMenu />
+            <div className="flex justify-end gap-3">
+                <CartDrawer />
+                <UserButton />
+            </div>
+        </nav>
+    )
+}
