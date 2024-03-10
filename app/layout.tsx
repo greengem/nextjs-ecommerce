69bda74bed2fab8dc776f4b6fdd59136e5d62b9e
Green Gem <chris_waitt@me.com>
2024-03-10 14:42:17 +0000

    Add Radix UI theme and styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Theme } from '@radix-ui/themes';
 import './globals.css'
+import '@radix-ui/themes/styles.css';
 import { Toaster } from 'sonner'
 //import NoticeMenu from '@/ui/Navbar/NoticeMenu';
 import Navbar from '@/ui/Navbar/Navbar';
@@ -23,11 +25,13 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className='flex flex-col min-h-dvh'>
         <body className={`${inter.className} flex flex-col grow`}>
-          <Toaster />
-          {/* <AdminMenu /> */}
-          {/* <NoticeMenu /> */}
-          <Navbar />
-          <main className='flex flex-col grow'>{children}</main>
+          <Theme accentColor="pink">
+            <Toaster />
+            {/* <AdminMenu /> */}
+            {/* <NoticeMenu /> */}
+            <Navbar />
+            <main className='flex flex-col grow'>{children}</main>
+          </Theme>
         </body>
       </html>
     </ClerkProvider>
