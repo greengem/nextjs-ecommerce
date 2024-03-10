8b4ab740a8ac8fad0976239fa6d4d909519cdd3e
Green Gem <chris_waitt@me.com>
2024-03-10 14:42:55 +0000

    Add Button component from Radix UI

--- ui/Generic/Button.tsx
+++ ui/Generic/Button.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { cn } from '@/lib/utils';
 import { useFormStatus } from 'react-dom'
+import { Button } from "@radix-ui/themes";
 
 type ButtonProps = {
     children: React.ReactNode;
@@ -10,23 +11,6 @@ type ButtonProps = {
     isIconOnly?: boolean;
     className?: string;
 };
-  
-export function Button({ children, onClick, type, disabled, isIconOnly, className }: ButtonProps) {
-  return (
-    <button
-      type={type || "button"}
-      className={cn(
-        "bg-neutral-100 hover:bg-neutral-400 text-black font-bold py-2 rounded text-xs ring-1 ring-neutral-400",
-        { "opacity-50": disabled, "px-2": isIconOnly, "px-4": !isIconOnly },
-        className
-      )}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      {children}
-    </button>
-  );
-}
 
 export function SubmitButton({ children }: ButtonProps) {
   const { pending } = useFormStatus()
